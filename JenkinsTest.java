282e288022e89595d141093eb3ce7bb06e577b15
Gautham Krishna <109744846+gtmcris@users.noreply.github.com>
2024-06-27 14:48:46 +0530

    Create JenkinsTest.java
    Adds two numbers (variables)

---
+++ JenkinsTest.java
@@ -0,0 +1,9 @@
+public class JenkinsTest {
+  public static void main(String[] main) {
+    int x = 2;
+    int y = 3;
+
+    int c = x + y;
+    System.out.println("The sum of x and y is: " + c);
+  }
+}
